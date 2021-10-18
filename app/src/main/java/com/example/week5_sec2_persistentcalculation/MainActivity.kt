3d2f3d4e1b08a27d49d29b9bfc07d4cc648552ac
Atheer-Alahmari <Atheer.a.alahmari@gmail.com>
2021-10-18 12:20:36 +0300

    Initial commit

--- app/src/main/java/com/example/week5_sec2_persistentcalculation/MainActivity.kt
+++ app/src/main/java/com/example/week5_sec2_persistentcalculation/MainActivity.kt
@@ -2,18 +2,39 @@ package com.example.week5_sec2_persistentcalculation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    var result =0.0
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         button.setOnClickListener {
             var dn=editTextDouble.text.toString().toDouble()
             var fn=editTextFloat.text.toString().toFloat()
-            var result=dn*fn
+             result=dn*fn
             textView.text="Result:\n $result"
 
         }
+    }
+
+    override fun onSaveInstanceState(outState : Bundle){
+        super.onSaveInstanceState(outState)
+        val input:Double=result
+        outState.putDouble("result",result)
+        // اذا كانت القيمة في ارري احط الارري داخل HashSet
+
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val input2:Double=savedInstanceState.getDouble("result",0.0)
+        result=input2
+        textView.text=result.toString()
+        // اطلعها من HashSet  و احولها ل ارري لست
+
     }
 }
